3eecc29657b2efb4b266a290ba37dd8566cdde54
Patrick Burke <denormative@gmail.com>
2017-04-06 12:08:30 +0900

    Update template

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,14 @@ module.exports = {
   plugins: [
     'html'
   ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
   // add your custom rules here
   'rules': {
     // don't require .vue extension when importing
